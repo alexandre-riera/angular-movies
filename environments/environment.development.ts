01aeac300f713f67e6a6415af21343cc77bb541b
alexandre-riera <alexandre.riera.dev@gmail.com>
2024-06-03 10:10:31 +0200

    Delete environments directory

--- environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    // baseUrl: "https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&language=fr-FR",
-    baseUrl: "https://api.themoviedb.org/3/discover/movie?include_adult=true&language=fr-FR",
-    apiKey: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODJkMGI4N2I3Y2ViNjVlZjdlYjk1YjdmYWZhZTU5NiIsInN1YiI6IjVlMTZmNTg1MWQwMTkxMDAxM2YxYTNlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.V4vWmPxkaliXTTy6ZohZvS-bzUCvZAf_qtDIxWyX07o"
-};
